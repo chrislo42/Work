075891274ef18e02354121087f6e6383b25c450e
chrislo42 <chris42.lopez@gmail.com>
2017-04-28 20:35:10 +0200

    modif pong pour centrage jeu

--- Html/Pong/js/game.js
+++ Html/Pong/js/game.js
@@ -16,11 +16,11 @@ var game = {
   failSound : null,
 
   init : function() {
-    this.groundLayer = game.display.createLayer("terrain", this.groundWidth, this.groundHeight, undefined, 0, "#000000", 0, 0);
+    this.groundLayer = game.display.createLayer("terrain", this.groundWidth, this.groundHeight, document.getElementById('myDiv'), 0, "#000000", 0, 0);
     game.display.drawRectangleInLayer(this.groundLayer, this.netWidth, this.groundHeight, this.netColor, this.groundWidth/2 - this.netWidth/2, 0);
-    this.scoreLayer = game.display.createLayer("score", this.groundWidth, this.groundHeight, undefined, 1, undefined, 0, 0);
+    this.scoreLayer = game.display.createLayer("score", this.groundWidth, this.groundHeight, document.getElementById('myDiv'), 1, undefined, 0, 0);
     this.displayScore(this.playerOne.score, this.playerTwo.score);
-    this.playersBallLayer = game.display.createLayer("joueursetballe", this.groundWidth, this.groundHeight, undefined, 2, undefined, 0, 0);
+    this.playersBallLayer = game.display.createLayer("joueursetballe", this.groundWidth, this.groundHeight, document.getElementById('myDiv'), 2, undefined, 0, 0);
     this.displayBall(200, 200);
     this.initPlayers();
     this.displayPlayers();
